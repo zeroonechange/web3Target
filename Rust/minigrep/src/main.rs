5ab7e9c12ff9291afef19006a22c2ebe4f7b9743
zeroonechange <zeroonechange@gmail.com>
2023-07-23 12:27:21 +0800

    add module and error handle

--- Rust/minigrep/src/main.rs
+++ Rust/minigrep/src/main.rs
@@ -1,12 +1,22 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 // cargo run -- test sample.txt
 fn main() {
     let args = env::args().collect::<Vec<String>>();
-    let query = &args[1];
-    let file_path = &args[2];
-    println!("cmd: {}  \t file_path: {}", query, file_path);
-    let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
-    println!("----------------------\n{}\n---------------------", contents);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("解析参数异常: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    // 简洁
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
