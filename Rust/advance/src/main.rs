29610526df4f7ef5e6ca8376df7e92c55698cb91
zeroonechange <zeroonechange@gmail.com>
2023-07-25 00:06:15 +0800

    rust 好难啊   所有权  智能指针   Box  Deref  Drop  Rc  Arc  Cell  RefCell

--- Rust/advance/src/main.rs
+++ Rust/advance/src/main.rs
@@ -1,3 +1,9 @@
+use std::cell::RefCell;
+
 fn main() {
-    println!("Hello, world!");
+    let s = RefCell::new(String::from("hello, world"));
+    let s1 = s.borrow();
+    let s2 = s.borrow_mut();
+
+    println!("{},{}", s1, s2);
 }
