202a46824a31cef888a66cd538456ddc727676db
zeroonechange <zeroonechange@gmail.com>
2023-07-31 23:00:43 +0800

    rust advance tutorial - server

--- Rust/advance/src/main.rs
+++ Rust/advance/src/main.rs
@@ -1,9 +1,26 @@
-use std::cell::RefCell;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
-    let s = RefCell::new(String::from("hello, world"));
-    let s1 = s.borrow();
-    let s2 = s.borrow_mut();
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
 
-    println!("{},{}", s1, s2);
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    neak_pointer();
+}
+
+fn neak_pointer() {
+    let mut num = 5;
+    let r1: *const i32 = &num as *const i32;
+    unsafe {
+        println!("r1 is {}", *r1);
+    }
 }
